93d6416d4e7d9991b9812cae7cc79720132e336a
TouseefDesigns <mirza.zia121@gmail.com>
2021-06-07 15:14:36 +0500

    Xpeer

---
+++ assets/js/custom.js
@@ -0,0 +1,25 @@
+// Sticky Navbar
+
+window.onscroll = function() {myFunction()};
+var navbar = document.getElementById("mynavbar");
+function myFunction() {
+  if (window.pageYOffset >= 100) {
+	navbar.classList.add("sticky")
+  } else {
+	navbar.classList.remove("sticky");
+  }
+}
+
+
+
+
+
+// window.onscroll = function() {myFunction()};
+// var navbar = document.getElementById("mynavbar");
+// function myFunction() {
+//   if (window.pageYOffset >= 100) {
+// 	navbar.classList.add("sticky")
+//   } else {
+// 	navbar.classList.remove("sticky");
+//   }
+// }
