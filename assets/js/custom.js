052717c89489363b1a8974777f7372b1e633f2ea
TouseefDesigns <mirza.zia121@gmail.com>
2021-06-08 23:51:37 +0500

    fixes

--- assets/js/custom.js
+++ assets/js/custom.js
@@ -11,15 +11,53 @@ function myFunction() {
 }
 
 
+$(document).ready(function(){
+
+    $(".filter-button").click(function(){
+        var value = $(this).attr('data-filter');
+        
+        if(value == "all")
+        {
+            //$('.filter').removeClass('hidden');
+            $('.filter').show('1000');
+        }
+        else
+        {
+//            $('.filter[filter-item="'+value+'"]').removeClass('hidden');
+//            $(".filter").not('.filter[filter-item="'+value+'"]').addClass('hidden');
+            $(".filter").not('.'+value).hide('3000');
+            $('.filter').filter('.'+value).show('3000');
+            
+        }
+    });
+
+});
 
 
 
-// window.onscroll = function() {myFunction()};
-// var navbar = document.getElementById("mynavbar");
-// function myFunction() {
-//   if (window.pageYOffset >= 100) {
-// 	navbar.classList.add("sticky")
-//   } else {
-// 	navbar.classList.remove("sticky");
+// $(document).ready(function(){
+
+//   $(".filter-button").click(function(){
+//       var value = $(this).attr('data-filter');
+      
+//       if(value == "all")
+//       {
+//           $('.filter').removeClass('hidden');
+//           $('.filter').show('1000');
+//       }
+//       else
+//       {
+//   //            $('.filter[filter-item="'+value+'"]').removeClass('hidden');
+//   //            $(".filter").not('.filter[filter-item="'+value+'"]').addClass('hidden');
+//           $(".filter").not('.'+value).hide('3000');
+//           $('.filter').filter('.'+value).show('3000');
+          
+//       }
+//   });
+  
+//   if ($(".filter-button").removeClass("active")) {
+//   $(this).removeClass("active");
 //   }
-// }
+//   $(this).addClass("active");
+  
+//   });
