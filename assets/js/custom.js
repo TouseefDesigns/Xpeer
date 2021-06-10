5d0996e9165e7edb1932f96964992233cb11d9b7
TouseefDesigns <mirza.zia121@gmail.com>
2021-06-10 15:36:24 +0500

    fixes

--- assets/js/custom.js
+++ assets/js/custom.js
@@ -1,45 +1,60 @@
 // Sticky Navbar
 
-window.onscroll = function() {myFunction()};
+window.onscroll = function () { myFunction() };
 var navbar = document.getElementById("mynavbar");
 function myFunction() {
   if (window.pageYOffset >= 100) {
-	navbar.classList.add("sticky")
+    navbar.classList.add("sticky")
   } else {
-	navbar.classList.remove("sticky");
+    navbar.classList.remove("sticky");
   }
 }
 
 
-$(document).ready(function(){
+$(document).ready(function () {
 
-    $(".filter-button").click(function(){
-        var value = $(this).attr('data-filter');
-        
-        if(value == "all")
-        {
-            //$('.filter').removeClass('hidden');
-            $('.filter').show('1000');
-        }
-        else
-        {
-//            $('.filter[filter-item="'+value+'"]').removeClass('hidden');
-//            $(".filter").not('.filter[filter-item="'+value+'"]').addClass('hidden');
-            $(".filter").not('.'+value).hide('3000');
-            $('.filter').filter('.'+value).show('3000');
-            
-        }
-    });
+  $(".filter-button").click(function () {
+    var value = $(this).attr('data-filter');
+    if (value == "all") {
+      //$('.filter').removeClass('hidden');
+      $('.filter').show('1000');
+    }
+    else {
+      //            $('.filter[filter-item="'+value+'"]').removeClass('hidden');
+      //            $(".filter").not('.filter[filter-item="'+value+'"]').addClass('hidden');
+      $(".filter").not('.' + value).hide('3000');
+      $('.filter').filter('.' + value).show('3000');
+    }
+// active
+    $(".filter-button").removeClass('active');
+    var $this = $(this);
+    if (!$this.hasClass('active')) {
+      $this.addClass('active');
+    }
 
+  });
 });
 
+
+
+
+// $(document).ready(function () {
+//   $(".filter-button").click(function (e) {
+//     $(".filter-button").removeClass('active');
+//     var $this = $(this);
+//     if (!$this.hasClass('active')) {
+//       $this.addClass('active');
+//     }
+//     //e.preventDefault();
+//   });
+// });
 
 
 // $(document).ready(function(){
 
 //   $(".filter-button").click(function(){
 //       var value = $(this).attr('data-filter');
-      
+
 //       if(value == "all")
 //       {
 //           $('.filter').removeClass('hidden');
@@ -51,13 +66,13 @@ $(document).ready(function(){
 //   //            $(".filter").not('.filter[filter-item="'+value+'"]').addClass('hidden');
 //           $(".filter").not('.'+value).hide('3000');
 //           $('.filter').filter('.'+value).show('3000');
-          
+
 //       }
 //   });
-  
+
 //   if ($(".filter-button").removeClass("active")) {
 //   $(this).removeClass("active");
 //   }
 //   $(this).addClass("active");
-  
+
 //   });
